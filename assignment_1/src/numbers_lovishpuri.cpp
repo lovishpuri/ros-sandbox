d9638bf0327db752ae79a315b71294299578cc86
lovishpuri <lovishpuri@gmail.com>
2015-03-15 19:43:51 +0530

    Create numbers_lovishpuri.cpp

---
+++ assignment_1/src/numbers_lovishpuri.cpp
@@ -0,0 +1,23 @@
++#include "ros/ros.h"
++#include "std_msgs/int64.msg"
++#include <sstream>
++
++int main(int argc, char **argv)
++{
++ros::init(argc, argv, "numbers");
++ ros::NodeHandle n;
++ ros::Publisher topic_numbers_pub = n.advertise<std_msgs::int64>("topics_numbers", 100);
++ros::Rate loop_rate(1);
++int count = 0;
++ while (ros::ok())
++ {
++ std_msgs::int64 msg;
++msg.data = count+1;
++ ROS_INFO("%d", msg.data);
++ topic_numbers_pub.publish(msg);
++ ;
++ loop_rate.sleep();
++ ++count;
++ }
++ return 0;
++ 
